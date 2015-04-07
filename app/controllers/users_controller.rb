7d4fa58efae0a714493de4b7942fa80d6d0b55fd
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-07 11:49:25 -0700

    add sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :require_not_signed_in, only: [:new, :create]
+
+  def index
+    # PLACEHOLDER
+
+    render :index
+  end
+
   def new
     @user = User.new()
     render :new
@@ -8,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      sign_in!
       redirect_to root_url
     else
       flash.now[:errors] = @user.errors.full_messages
