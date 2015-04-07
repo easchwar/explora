806571cc37c270f6a8be9ec813d25daebfdb65d9
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-07 13:55:18 -0700

    initialize backbone

---
+++ app/assets/javascripts/explora.js
@@ -0,0 +1,13 @@
+window.Explora = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    // alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Explora.initialize();
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
+
+  namespace :api do
+    resources :questions, only: [:index, :create, :update, :destroy]
+  end
 end
