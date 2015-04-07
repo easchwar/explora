c57fcaee6f84a4bd19cae2657ef3c5d908e441f0
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-07 11:15:54 -0700

    add user controller and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,47 @@
 
 class User < ActiveRecord::Base
   attr_reader :password
+
   validates :username, :password_digest, :session_token, presence: true
-  
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    token = SecureRandom.urlsafe_base64(16)
+
+    while User.exists?(session_token: token)
+      token = SecureRandom.urlsafe_base64(16)
+    end
+
+    token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if !user
+
+    user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
