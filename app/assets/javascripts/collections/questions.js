5c25aaccd8853139e002011088f73f53343a8803
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-07 15:55:10 -0700

    add getorfetch

--- app/assets/javascripts/collections/questions.js
+++ app/assets/javascripts/collections/questions.js
@@ -1,4 +1,20 @@
 Explora.Collections.Questions = Backbone.Collection.extend({
   url: '/api/questions',
   model: Explora.Models.Question,
+
+  getOrFetch: function(id) {
+    var question;
+    if (this.get(id)) {
+      question = this.get(id);
+    } else {
+      question = new Explora.Models.Question({id: id});
+    }
+    question.fetch({
+      success: function(model) {
+        this.add(model);
+      }.bind(this)
+    });
+
+    return question;
+  }
 });
