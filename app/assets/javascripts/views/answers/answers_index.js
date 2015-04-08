ba58dd3448978c2a2bf472a0c7c7c2b259b8210e
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-08 14:12:35 -0700

    add answer views

---
+++ app/assets/javascripts/views/answers/answers_index.js
@@ -0,0 +1,37 @@
+Explora.Views.AnswersIndex = Backbone.CompositeView.extend({
+  template: JST['answers/index'],
+
+  initialize: function() {
+    this.addAllItems();
+
+    this.listenTo(this.collection, 'sync', this.render);
+    this.listenTo(this.collection, 'add', this.addIndexItem);
+    this.listenTo(this.collection, 'remove', this.removeIndexItem);
+  },
+
+  addAllItems: function() {
+    this.collection.each(function(model) {
+      this.addIndexItem(model);
+    }, this);
+  },
+
+  addIndexItem: function(model) {
+    var view = new Explora.Views.AnswersIndexItem({model: model});
+    this.addSubview('.answers-index', view);
+  },
+
+  removeIndexItem: function(model) {
+    this.subviews('.answers-index').forEach(function(subview) {
+      if (subview.model === model) {
+        this.removeSubview('.answers-index', subview);
+      }
+    }.bind(this));
+  },
+
+  render: function() {
+    var content = this.template();
+    this.$el.html(content);
+    this.attachSubviews();
+    return this;
+  },
+});
---
+++ app/assets/javascripts/views/answers/answers_index_item.js
@@ -0,0 +1,24 @@
+Explora.Views.AnswersIndexItem = Backbone.View.extend({
+  template: JST['answers/index_item'],
+
+  tagName: 'li',
+  className: 'list-group-item',
+
+  events: {
+    'click .item-delete': 'deleteItem'
+  },
+
+  initialize: function() {
+    this.listenTo(this.model, 'sync', this.render);
+  },
+  
+  render: function() {
+    var content = this.template({answer: this.model});
+    this.$el.html(content);
+    return this;
+  },
+
+  deleteItem: function(event) {
+    this.model.destroy();
+  },
+});
