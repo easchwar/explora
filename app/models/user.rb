bb5e788842a9728c5f4ea0ae4d97b24e3dd5a6c5
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-09 15:13:02 -0700

    add session_token uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   attr_reader :password
 
   validates :username, :password_digest, :session_token, presence: true
-  validates :username, uniqueness: true
+  validates :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   has_many :questions, foreign_key: :author_id, dependent: :destroy
