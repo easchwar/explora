1f6e030a8ffbe816545936d69da81cb78c3b0b1d
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-09 11:52:33 -0700

    create seed data taggings on questions creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,22 +14,13 @@ u = User.create!(username: 'user1', password: 'password')
 u2 = User.create!(username: 'user2', password: 'password')
 
 
-q1 = e.questions.create!(body: 'who?')
-q2 = e.questions.create!(body: 'what?')
-q3 = e.questions.create!(body: 'where?')
+q1 = e.questions.create!(body: 'who?', tag_ids: [f.id, a.id, t.id])
+q2 = e.questions.create!(body: 'what?', tag_ids: [a.id, s.id, t.id])
+q3 = e.questions.create!(body: 'where?', tag_ids: [a.id])
 
-u.questions.create!(body: 'user1 question1')
-u.questions.create!(body: 'user1 question2')
+u.questions.create!(body: 'user1 question1', tag_ids: [f.id, s.id, a.id])
+u.questions.create!(body: 'user1 question2', tag_ids: [f.id, s.id, t.id])
 
 q1.answers.create!(body: 'me', author_id: u.id)
 q1.answers.create!(body: 'and you', author_id: u2.id)
 q2.answers.create!(body: 'nothing', author_id: u.id)
-
-Tagging.create!(tag_id: f.id, question_id: q1.id)
-Tagging.create!(tag_id: s.id, question_id: q1.id)
-Tagging.create!(tag_id: t.id, question_id: q1.id)
-Tagging.create!(tag_id: a.id, question_id: q1.id)
-Tagging.create!(tag_id: a.id, question_id: q2.id)
-Tagging.create!(tag_id: t.id, question_id: q2.id)
-Tagging.create!(tag_id: t.id, question_id: q3.id)
-Tagging.create!(tag_id: f.id, question_id: q3.id)
